525b11ab5e40baa4027ee8f5968a7f4de4e0e5fe
Victor Mwendwa <victormwendwa804@gmail.com>
2024-07-18 19:54:11 +0300

    Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,6 +27,9 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    if user_id == 'me':
+        if request.current_user is None:
+            abort(404)
     user = User.get(user_id)
     if user is None:
         abort(404)
@@ -120,3 +123,11 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
+
+
+@app_views.route("/user/me", methods=['GET'], strict_slashes=False)
+def get_aunthenticated_user():
+    """ method to get authenticated user """
+    if request.current_user is None:
+        abort(404)
+        return jsonify(request.current_user.to_json())
